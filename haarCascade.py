5cc944d1d7337d9c9ca81b93221bf5167780f31e
chriskarouta <ckarouta@utexas.edu>
2022-02-07 11:18:14 -0600

    Update haarCascade.py

--- haarCascade.py
+++ haarCascade.py
@@ -2,50 +2,35 @@ from __future__ import print_function
 import cv2 as cv
 import argparse
 def detectAndDisplay(frame):
+    # greyscale for faster detection
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frame_gray = cv.equalizeHist(frame_gray)
     #-- Detect faces
-    faces = face_cascade.detectMultiScale(frame_gray)
+    faces = face_cascade.detectMultiScale(frame_gray, 1.1, 3)
     for (x,y,w,h) in faces:
         center = (x + w//2, y + h//2)
         frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
-        faceROI = frame_gray[y:y+h,x:x+w]
-        #-- In each face, detect eyes
-        eyes = eyes_cascade.detectMultiScale(faceROI)
-        for (x2,y2,w2,h2) in eyes:
-            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-            radius = int(round((w2 + h2)*0.25))
-            frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
-    cv.imshow('Capture - Face detection', frame)
-parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-# cv.samples.addSamplesDataSearchPath('C:/Users/ckaro/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0/LocalCache/local-packages/Python310/site-packages/cv2/data')
-# parser.add_argument('--face_cascade', default='haarcascades/haarcascade_frontalface_alt.xml')
-# parser.add_argument('--eyes_cascade', default='haarcascades/haarcascade_eye_tree_eyeglasses.xml')
-parser.add_argument('--face_cascade', help='Path to face cascade.', default='C:/Users/ckaro/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0/LocalCache/local-packages/Python310/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
-parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='C:/Users/ckaro/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0/LocalCache/local-packages/Python310/site-packages/cv2/data/haarcascade_eye_tree_eyeglasses.xml')
-parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
-args = parser.parse_args()
-print("here               NNNNNNNNNNN" + args.face_cascade)
-face_cascade_name = args.face_cascade
-eyes_cascade_name = args.eyes_cascade
-face_cascade = cv.CascadeClassifier()
-eyes_cascade = cv.CascadeClassifier()
-#-- 1. Load the cascades
-if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
-    print('--(!)Error loading face cascade')
-    exit(0)
-if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
-    print('--(!)Error loading eyes cascade')
-    exit(0)
-camera_device = args.camera
+    # Detect upper bodies
+    bodies = body_cascade.detectMultiScale(frame_gray, 
+        scaleFactor = 1.1,
+        minNeighbors =  3, 
+        minSize = (50, 100), 
+        flags = cv.CASCADE_SCALE_IMAGE
+    )
+    for(x,y,w,h) in bodies:
+        frame = cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv.imshow('Capture - Face and body detection', frame)
+face_cascade = cv.CascadeClassifier('C:/Users/ckaro/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0/LocalCache/local-packages/Python310/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
+body_cascade = cv.CascadeClassifier('C:/Users/ckaro/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0/LocalCache/local-packages/Python310/site-packages/cv2/data/haarcascade_upperbody.xml')
 #-- 2. Read the video stream
-# cap = cv.VideoCapture(camera_device)
 cap = cv.VideoCapture(0)
 if not cap.isOpened:
     print('--(!)Error opening video capture')
     exit(0)
 while True:
     ret, frame = cap.read()
+    # resizing for faster detection
+    frame = cv.resize(frame, (640, 480))
     if frame is None:
         print('--(!) No captured frame -- Break!')
         break
